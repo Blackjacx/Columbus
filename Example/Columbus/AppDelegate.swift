25dd837084f2e67896d74c1f7ee661127829ccc3
Stefan Herold <stefan.herold@gmail.com>
2018-11-14 17:13:00 +0100

    Add customizable separator instes

--- Example/Columbus/AppDelegate.swift
+++ Example/Columbus/AppDelegate.swift
@@ -18,14 +18,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
 
-        let config = DefaultConfig()
+        let config = CountryPickerConfig()
         Columbus.config = config
 
-        let countryPicker = CountryPickerViewController(initialRegionCode: "DE", didSelectClosure: { [weak self] (country) in
+        let countryPicker = CountryPickerViewController(initialRegionCode: "DE", didSelectClosure: { (country) in
             print(country)
         })
         window?.rootViewController = countryPicker
         window?.makeKeyAndVisible()
         return true
     }
+}
+
+public struct CountryPickerConfig: Configuration {
+    public var textColor: UIColor = .darkGray
+    public var textAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 20)]
+    public var lineColor: UIColor = .lightGray
+    public var lineWidth: CGFloat = 1.0 / UIScreen.main.scale
+    public var rasterSize: CGFloat = 12.0
+    public var backgroundColor: UIColor = .white
+    public var separatorInsets: UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: rasterSize * 3.7, bottom: 0, right: rasterSize)
+    }
+    public var controlColor: UIColor = UIColor(red: 1.0/255.0, green: 192.0/255.0, blue: 1, alpha: 1)
+    public var searchBarPlaceholder: String? = "Search"
+
+    public init() {}
 }
