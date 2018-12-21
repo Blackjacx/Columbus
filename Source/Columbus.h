ce5224d5a4b666f594b569eddbe58cd467e1ff88
Stefan Herold <stefan.herold@gmail.com>
2018-12-21 16:39:29 +0100

    Enable Carthage Support
    Refactoring folder and project structure to this format not only enables
    carthage support but also makes it easier to work with the examples.
    At the same time CocoaPods support is maintained.

---
+++ Source/Columbus.h
@@ -0,0 +1,19 @@
+//
+//  Columbus.h
+//  Columbus
+//
+//  Created by Stefan Herold on 21.12.18.
+//  Copyright © 2018 CodingCobra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Columbus.
+FOUNDATION_EXPORT double ColumbusVersionNumber;
+
+//! Project version string for Columbus.
+FOUNDATION_EXPORT const unsigned char ColumbusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Columbus/PublicHeader.h>
+
+
